64eb4f3bdf66e199ae726618cf8195e33cd65470
Ayushman Mishra <ayushmanmishra094@gmail.com>
2023-07-14 20:13:17 +0530

    last commit- added if statement for bye

--- Robot_Speaker/main.py
+++ Robot_Speaker/main.py
@@ -4,5 +4,8 @@ if __name__ == '__main__':
     print("Welcome to Robot Speaker 1.1. Created by Ayushman")
     while True:
         x = input("Enter your speech: ")
+        if x == "q":
+            os.system("say 'bye bye friend'")
+            break
         command = f'say "{x}"'
         os.system(command)
