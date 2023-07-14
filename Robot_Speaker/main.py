3a4194d70ddb411457785d696037f7d6b0cf5293
Ayushman Mishra <ayushmanmishra094@gmail.com>
2023-07-14 20:11:48 +0530

    while loop added- running command

--- Robot_Speaker/main.py
+++ Robot_Speaker/main.py
@@ -2,3 +2,7 @@ import os
 
 if __name__ == '__main__':
     print("Welcome to Robot Speaker 1.1. Created by Ayushman")
+    while True:
+        x = input("Enter your speech: ")
+        command = f'say "{x}"'
+        os.system(command)
